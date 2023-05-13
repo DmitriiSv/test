b4b5395d3bcd06c3e8c06247e1d34dcdcebca8cf
Dmitrii Sverlov <dmitry@sverlov.com>
2023-05-13 12:45:03 +0300

    Second commit

--- InsertDB.sql
+++ InsertDB.sql
@@ -95,59 +95,138 @@ UPDATE albums
 SET release_year = 1987
 WHERE id = 2;
 
-INSERT INTO tracks (аlbums_id, name, duration) 
+INSERT INTO tracks (аlbums_id, name, duration)
 VALUES(1, 'Circus', '3:12');
+
+UPDATE tracks
+SET duration = '00:03:12'
+WHERE id = 1;
 
 INSERT INTO tracks (аlbums_id, name, duration) 
 VALUES(1, 'My Baby', '3:22');
 
+UPDATE tracks
+SET duration = '00:03:22'
+WHERE id = 2;
+
+
 INSERT INTO tracks (аlbums_id, name, duration) 
 VALUES(2, 'Bad', '4:07');
+
+UPDATE tracks
+SET duration = '00:04:07'
+WHERE id = 3;
 
 INSERT INTO tracks (аlbums_id, name, duration) 
 VALUES(2, 'Leave Me Alone', '4:30');
 
+UPDATE tracks
+SET duration = '00:04:30'
+WHERE id = 4;
+
 INSERT INTO tracks (аlbums_id, name, duration) 
 VALUES(3, 'In My Place', '3:50');
+
+UPDATE tracks
+SET duration = '00:03:50'
+WHERE id = 5;
 
 INSERT INTO tracks (аlbums_id, name, duration) 
 VALUES(3, 'See You Soon', '3:08');
 
+UPDATE tracks
+SET duration = '00:03:08'
+WHERE id = 6;
+
 INSERT INTO tracks (аlbums_id, name, duration) 
 VALUES(3, 'Trouble', '4:24');
+
+UPDATE tracks
+SET duration = '00:04:24'
+WHERE id = 7;
 
 INSERT INTO tracks (аlbums_id, name, duration) 
 VALUES(4, 'Fuh You', '3:25');
 
+UPDATE tracks
+SET duration = '00:03:25'
+WHERE id = 8;
+
 INSERT INTO tracks (аlbums_id, name, duration) 
 VALUES(4, 'Do It Now', '3:17');
+
+UPDATE tracks
+SET duration = '00:03:17'
+WHERE id = 9;
 
 INSERT INTO tracks (аlbums_id, name, duration) 
 VALUES(4, 'Back In Brazil', '3:20');
 
+UPDATE tracks
+SET duration = '00:03:20'
+WHERE id = 10;
+
 INSERT INTO tracks (аlbums_id, name, duration) 
 VALUES(5, 'Pilgrim', '4:17');
+
+UPDATE tracks
+SET duration = '00:04:17'
+WHERE id = 11;
 
 INSERT INTO tracks (аlbums_id, name, duration) 
 VALUES(5, 'My Fathers Eyes', '5:24');
 
+UPDATE tracks
+SET duration = '00:05:24'
+WHERE id = 12;
+
 INSERT INTO tracks (аlbums_id, name, duration) 
 VALUES(5, 'Circus', '4:10');
+
+UPDATE tracks
+SET duration = '00:04:10'
+WHERE id = 13;
 
 INSERT INTO tracks (аlbums_id, name, duration) 
 VALUES(6, 'Bad News', '3:25');
 
+UPDATE tracks
+SET duration = '00:03:25'
+WHERE id = 14;
+
 INSERT INTO tracks (аlbums_id, name, duration) 
 VALUES(7, 'This Is My Love', '3:30');
+
+UPDATE tracks
+SET duration = '00:03:30'
+WHERE id = 15;
 
 INSERT INTO tracks (аlbums_id, name, duration) 
 VALUES(8, 'All Day', '3:06');
 
+UPDATE tracks
+SET duration = '00:03:06'
+WHERE id = 16;
+
 INSERT INTO tracks (аlbums_id, name, duration) 
 VALUES(8, 'Lonely', '2:46');
 
+UPDATE tracks
+SET duration = '00:02:46'
+WHERE id = 17;
+
 INSERT INTO tracks (аlbums_id, name, duration) 
 VALUES(8, 'Wild Flower', '4:33');
+
+UPDATE tracks
+SET duration = '00:04:33'
+WHERE id = 18;
+
+INSERT INTO tracks (аlbums_id, name, duration) 
+VALUES(2, 'Myself', '00:02:33');
+
+INSERT INTO tracks (аlbums_id, name, duration) 
+VALUES(2, 'EneMy', '00:02:37');
 
 INSERT INTO albumsartists (albums_id, artists_id) 
 VALUES(1, 1);
@@ -270,3 +349,4 @@ VALUES(8, 2);
 
 INSERT INTO Сollectionstracks (collections_id, tracks_id) 
 VALUES(8, 3);
+
